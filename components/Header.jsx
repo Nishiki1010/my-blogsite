68b6c3c5fc05bfdd66ba46f60fc2bc3ec8655259
Nishiki1010 <integral0519@gmail.com>
2022-12-05 15:41:05 +0900

    ヘッダー変更

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,14 +3,14 @@ import Link from "next/link"
 
 const Header = () => {
   return (
-    <div>
-        <div>
-            {/* <span>Nishiki Blog</span>ここをPropsで受け取りつつ値を変化させてページをわかりやすくする */}
-        </div>
-        <div>
-            <Link href="/">HOME</Link>
-            <Link href="/about">about</Link>
-            <Link href="/">skill</Link>
+    <div className='text-gray-700 border-b border-gray-300'>
+        <div className='container flex mx-auto p-5 flex-col md-'>
+            <span className='font-medium ml-3 text-xl'>Nishiki Blog</span>
+            <div className='flex md:ml-auto items-center justify-center text-base'>
+                <Link className="mr-5 hover:text-blue-400 duration-300" href="/">HOME</Link>
+                <Link className="mr-5 hover:text-blue-400 duration-300" href="/about">about</Link>
+                <Link className="mr-5 hover:text-blue-400 duration-300" href="/">skill</Link>
+            </div>
         </div>
     </div> 
   )
